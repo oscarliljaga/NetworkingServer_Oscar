d9442f2b8c63845b29a2a5ee16c97142cd9d1233
oscarliljaga <oscar.lilja@elev.ga.ntig.se>
2024-05-23 10:45:45 +0200

    Första commit

---
+++ src/NetworkingServer.java
@@ -0,0 +1,44 @@
+import java.net.*;
+import java.io.*;
+
+public class NetworkingServer {
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket client;
+
+        int portnumber = 1234;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        try {
+            server = new ServerSocket(portnumber);
+        } catch (IOException ie) {
+            System.out.println("Cannot open socket " + ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created " + server);
+
+        while (true) {
+            try {
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+
+                System.out.println("Connect request accepted...");
+                String clientHost = client.getInetAddress().getHostAddress();
+                int clientPort = client.getPort();
+                System.out.println("Client host = " + clientHost + "\nClient port = " + clientPort);
+
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                String msgFromClient = br.readLine();
+                System.out.println("Message from client: " + msgFromClient);
+
+                if(msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")) {
+                    OutputStream clientOut = client.getOutputStream();
+                    PrintWriter pw = new PrintWriter(clientOut, true);
+                }
+            }
+        }
+    }
+}
